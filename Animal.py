1ad90dca17787e516ae7a0f7d4d1653d3b6525f7
Willie Zou <williezou@Willies-MacBook-Air.local>
2025-02-24 19:01:35 -0500

    5 Functions

--- Animal.py
+++ Animal.py
@@ -1,8 +1,27 @@
 class Animal:
-    def __init__(self, name):
-        self.__name = name 
-        print("hello, I am", self.__name)
+    def __init__(self, name): 
+        self._name = name
+        print("Hello, I am", self._name)
 
     def talk(self):
-        print("hi")
+        print("Hi, I am a animal")
 
+    def eat(self, eat):
+        self._eat = eat
+        print("I am eating a", self._eat)
+
+    def species(self, species):
+        self._species = species
+        print("I am a", self._species)
+
+    def sleep(self, sleeping):
+        self._sleep = sleeping
+        print("I am sleeping in a", self._sleep)
+
+    def fight(self, fight):
+        self._fight = fight
+        print("I am fighting a ", self._fight)
+
+    def flee(self, escape):
+        self._escape = escape
+        print("I am escaping from a", self._escape)
